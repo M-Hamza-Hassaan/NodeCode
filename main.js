d955f49defe4e730fc5e750f7acc29a0a32f6c1e
CodeNode <mhhassaan.1@gmail.com>
2024-11-05 15:13:51 +0500

    Day 5

--- main.js
+++ main.js
@@ -219,10 +219,30 @@
 // NODE.JS URL MODULE
 
 // PART 14
+// var http = require('http');
+// var url = require('url')
+// var addr = 'https://github.com/Code-Damn-Hamza/NodeCode';
+// var q = url.parse(addr, true);
+// console.log(q.host); //returns 'github.com'
+// console.log(q.pathname); //returns '/Code-Damn-Hamza'
+// console.log(q.search); 
+
+//PART 15
 var http = require('http');
-var url = require('url')
-var addr = 'https://github.com/Code-Damn-Hamza/NodeCode';
-var q = url.parse(addr, true);
-console.log(q.host); //returns 'github.com'
-console.log(q.pathname); //returns '/Code-Damn-Hamza'
-console.log(q.search); 
+var url = require('url');
+var fs = require('fs');
+
+http.createServer(function (req, res) {
+  var q = url.parse(req.url, true);
+    console.log(q);
+    var filename = "." + q.pathname;
+  fs.readFile(filename, function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    } 
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    return res.end();
+  });
+}).listen(8080,function(){console.log("SERVER IS LIVE AT 8080.")});
