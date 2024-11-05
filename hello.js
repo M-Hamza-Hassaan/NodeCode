003b7b1d59952f743f60058b747057b09f2c011b
CodeNode <mhhassaan.1@gmail.com>
2024-11-05 14:12:30 +0500

    Day 3

--- hello.js
+++ hello.js
@@ -98,7 +98,7 @@
 
 //PART-8
 
-// What i did, i just used readfile on newly created file, just for practice, must do that
+// What i did, i just used "readfile" on newly created file, just for practice, must do that
 
 // var http = require ('http');
 // var fs = require('fs');
@@ -134,11 +134,25 @@
 // 'a' for appending new content without deleting existing content.
 // 'w' for writing, which will clear any existing content.
 
+
+
+// PART 10
+
 // var http = require ('http');
 // var fs = require('fs');
 
 // http.createServer(function(req, res){
-//     fs.open('myNewFile1.txt', 'w',function(err, fileDescriptor){
+//     fs.open('myNewFile1.txt', 'r',function(err, fileDescriptor){
+//         if(err){
+//             res.writeHead(500, { 'Content-Type': 'text/plain' });
+//             res.write('Internal Server Error: File could not be opened.');
+//             return res.end();
+//         }
+//         res.writeHead(200, {'Content-Type' : 'text/html'});
+//         res.write("The file has been opened successfully.");
+//         res.end();
+//     })
+//     fs.appendFile('myNewFile1.txt' , 'Hello Guyz, Chai pi lo' , function(err){
 //         if(err){
 //             res.writeHead(500, { 'Content-Type': 'text/plain' });
 //             res.write('Internal Server Error: File could not be opened.');
@@ -149,3 +163,35 @@
 //         res.end();
 //     })
 // }).listen(8080, function(){console.log('Server is listening on port 8080')});
+
+//PART 11
+
+// var http = require('http');
+// var fs = require('fs');
+
+// http.createServer(function(req, res){
+//     fs.appendFile('myNewFile1.txt' , ' THE NEW TEXT' , function(err){
+//         if(err){
+//             res.writeHead(500, {'Content-type': 'text/html'});
+//             res.write("Error Bro");
+//             return res.end();
+//         }
+//         res.writeHead(200, {'Content-type': 'text/html'});
+//             res.write("Ho gea Bro");
+//             return res.end();
+//     })
+// }).listen(8080, function(){console.log('Server is live on local port 8080')});
+
+
+//PART 12
+var http = require('http');
+var fs = require('fs');
+
+http.createServer(function(req, res){
+    fs.unlink('myNewFile1.txt' , function(err){
+        if(err){
+            res.writeHead(500, {'Content-type': 'text/html'});
+            res.write("Error Bro");
+            return res.end();
+    }})
+}).listen(8080, function(){console.log('Server py dekh byy')});
