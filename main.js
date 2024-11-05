abf90135250ac68138ca70b7e734670f2c681430
CodeNode <mhhassaan.1@gmail.com>
2024-11-05 14:56:22 +0500

    Day 4

--- main.js
+++ main.js
@@ -199,22 +199,30 @@
 
 
 //PART 13
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-http.createServer(function(req, res){
+// http.createServer(function(req, res){
     
-    fs.rename('myNewFile1.txt','Renamed.txt', (err) => {
-        if (err) {
-            res.writeHead(500, { 'Content-Type': 'text/html' });
-            res.write("Error Bro");
-            return res.end();
-        }
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        res.write("Ho gea Bro");
-        return res.end();
-    });
-}).listen(8080, function(){console.log('Server py dekhen sir')});
+//     fs.rename('myNewFile1.txt','Renamed.txt', (err) => {
+//         if (err) {
+//             res.writeHead(500, { 'Content-Type': 'text/html' });
+//             res.write("Error Bro");
+//             return res.end();
+//         }
+//         res.writeHead(200, { 'Content-Type': 'text/html' });
+//         res.write("Ho gea Bro");
+//         return res.end();
+//     });
+// }).listen(8080, function(){console.log('Server py dekhen sir')});
 
+// NODE.JS URL MODULE
 
-
+// PART 14
+var http = require('http');
+var url = require('url')
+var addr = 'https://github.com/Code-Damn-Hamza/NodeCode';
+var q = url.parse(addr, true);
+console.log(q.host); //returns 'github.com'
+console.log(q.pathname); //returns '/Code-Damn-Hamza'
+console.log(q.search); 
