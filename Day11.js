a922b5ca9d6a9e6de5180b0d01747c201d571ae3
CodeNode <mhhassaan.1@gmail.com>
2024-11-21 13:36:33 +0500

    Day 11

---
+++ Day11.js
@@ -0,0 +1,23 @@
+// // Part 19
+
+// var os = require('os')
+
+// // os.uptime
+// // This function returns the number of seconds the system has been running since it was last rebooted. 
+// const SystemUpTime = os.uptime();
+
+// // os.userInfo
+// const userInfo = os.userInfo();
+
+// // otherInfos, that must be in objsct
+// const otherInfos = {name: os.type(),
+//     release: os.release(),
+//     totalMem: os.totalmem(),
+//     freeMem: os.freemem(),
+// }
+
+// console.log(SystemUpTime);
+// console.log(userInfo);
+// console.log(otherInfos);
+
+// // Part 20
