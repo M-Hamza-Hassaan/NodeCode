d7a40d0225724895c606d22a2edbba654a91e3d7
CodeNode <mhhassaan.1@gmail.com>
2024-11-26 15:44:24 +0500

    Day 14

--- RESTful_Day_12/RESTful.js
+++ RESTful_Day_12/RESTful.js
@@ -40,13 +40,13 @@ app.get("/api/users" , (req, res) => {
 
 
 
-app.post("/api/users" , (req, res) => {
-    const body = req.body;
-    users.push({...body, id: users.length+1});
-    fs.writeFile('MOCK_DATA.json' , JSON.stringify(users), (err, data)=>{
-      return res.json({status:"success", message:"User added successfully"});
-    })
-});
+
+
+
+// app.post("/api/users/:id" , (req, res) => {
+//     return res.json({status:"pending"});
+// });
+
 
 // app.patch("/api/users/:id" , (req, res) => {
 //     return res.json({status:"pending"});
@@ -59,9 +59,32 @@ app.post("/api/users" , (req, res) => {
 
 
 
+// app.route("/api/users/:id")
+
+// .get((req,res) =>{
+//     const id = Number(req.params.id);
+//     const user = users.find((user) => user.id === id);
+//     return res.json(user);
+// })
+// .patch((req, res) => {
+//     return res.json({status:"pending"});
+// })
+// .delete((req, res) => {
+//     return res.json({status:"pending"});
+// })
+
+
+
+app.post("/api/users" , (req, res) => {
+    const body = req.body;
+    users.push({...body, id: users.length+1});
+    fs.writeFile('MOCK_DATA.json' , JSON.stringify(users), (err, data)=>{
+      return res.json({status:"success", message:"User added successfully"});
+    })
+});
+
+
 app.route("/api/users/:id")
-
-
 
 .get((req,res) =>{
     const id = Number(req.params.id);
