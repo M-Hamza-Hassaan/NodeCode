298e3615afaa5cb1179c31a9a5cf8d72fd762fd1
CodeNode <mhhassaan.1@gmail.com>
2024-11-30 15:22:15 +0500

    Day 17

--- RESTful_Day_12-18/middleware.js
+++ RESTful_Day_12-18/middleware.js
@@ -8,73 +8,26 @@ const PORT = 8000
 //Middleware to parse the body of the request
 app.use(express.urlencoded({extended:false}));
 
-//ROUTES
-app.get('/users' , (req, res) => {
-    // return res.json(users);
-    const html = `<ul>
-    ${users.map((user) => `<li>My name is ${user.first_name}</li>`).join("")}
-    </ul>`;
-    return res.send(html);
+app.use((req, res, next)=>{
+    console.log("this is middleware 1");
+    next(); 
 })
-//GET ALL USERS IN HTML SIMPLYY....
+app.use((req, res, next)=>{
+    console.log("this is middleware 2");
+    return res.end("hey");
+})
 
-//APPLYING RESTAPI TO MAKE IT FEASIBLE FOR ALL USERS
+
+app.use((req, res, next)=>{
+    fs.appendFile("midWare.txt", `${Date.now()}`, (err, data)=> {
+        // next();
+    })
+})
+
+
 app.get("/api/users" , (req, res) => {
     return res.json(users);
-    // const html = `<ul>
-    // ${users.map((user) => `<li>My name is ${user.first_name}</li>`).join("")}
-    // </ul>`;
-    // return res.send(html);
 });
-// GET ALL USERS---DONE
-
-// app.get("/api/users/:id" , (req,res) =>{
-//     const id = Number(req.params.id);
-//     const user = users.find((user) => user.id === id);
-//     // return res.json(user);
-
-//     const html = `<li>My name is ${user.first_name}</li>`;
-//     return res.send(html)
-// })
-//GET USER BY ID---DONE
-
-
-
-
-
-
-
-// app.post("/api/users/:id" , (req, res) => {
-//     return res.json({status:"pending"});
-// });
-
-
-// app.patch("/api/users/:id" , (req, res) => {
-//     return res.json({status:"pending"});
-// });
-
-// app.delete("/api/users/:id" , (req, res) => {
-//     return res.json({status:"pending"});
-// });
-
-
-
-
-// app.route("/api/users/:id")
-
-// .get((req,res) =>{
-//     const id = Number(req.params.id);
-//     const user = users.find((user) => user.id === id);
-//     return res.json(user);
-// })
-// .patch((req, res) => {
-//     return res.json({status:"pending"});
-// })
-// .delete((req, res) => {
-//     return res.json({status:"pending"});
-// })
-
-
 
 app.post("/api/users" , (req, res) => {
     const body = req.body;
@@ -93,6 +46,7 @@ app.route("/api/users/:id")
     const user = users.find((user) => user.id === id);
     return res.json(user);
 })
+
 
 .patch((req, res) => {
     const id = Number(req.params.id); 
@@ -123,8 +77,6 @@ app.route("/api/users/:id")
     return res.json({status:"success", message:"User removed successfully"});
 });
 });
-
-
 
 app.listen(PORT, ()=>{
     console.log(`Server is running on ${PORT}`)
